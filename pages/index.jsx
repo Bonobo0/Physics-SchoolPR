7446a89619da4a341340de61a5188cf782b83bef
bonobo <47466832+Bonobo0@users.noreply.github.com>
2022-05-19 13:44:28 +0900

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -351,7 +351,7 @@ export default function Index() {
           밀대 힘
           <Slider aria-label="Custom marks" max={0.1428} min={0} step={0.000000001} defaultValue={0.1} valueLabelDisplay="auto" onChange={(e, val) => setPusherForceStateValue(val)} />
           밀대 질량
-          <Slider aria-label="Custom marks" max={100} min={0} step={0.1} defaultValue={1} valueLabelDisplay="auto" onChange={(e, val) => setPusherForceStateValue(val)} />
+          <Slider aria-label="Custom marks" max={100} min={0} step={0.1} defaultValue={1} valueLabelDisplay="auto" onChange={(e, val) => setPusherMassStateValue(val)} />
           공 질량
           <Slider aria-label="Custom marks" max={10} min={0} step={0.000000001} defaultValue={0.1} valueLabelDisplay="auto" onChange={(e, val) => setBallMassStateValue(val)} />
           공 마찰력
